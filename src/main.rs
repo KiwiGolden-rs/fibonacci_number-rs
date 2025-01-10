b1988b499c36d535f17fdfc9c5c32d46c1aad5d3
KiwiGoleden-rs <QS.object@proton.me>
2025-01-10 12:18:25 +0100

    Init Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 0..=20 {
+        println!("Fibonacci ({}) => {}",i, fibonacci(i));
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
